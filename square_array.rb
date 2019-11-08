7e616a3be99b68125d58e3e742f4ebdfef2bd9d8
connor worthen <connorw220@protonmail.com>
2019-11-08 20:47:51 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
-  array.each_with_object([]) { |i,arr| arr << i ** 2 }
+  array.each_with_object([]) { |i,arr| array << i ** 2 }
 end
 
 my_array = ["1", "2", "3"]
-p square_array(my_array)    
+p square_array(my_array)
