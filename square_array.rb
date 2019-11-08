3ac529428a5c25d33784e7eeb74ac79ef91ea1fc
connor worthen <connorw220@protonmail.com>
2019-11-08 21:49:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,6 @@
   def square_array(array)
-  square_array = [1, 2, 3, 4, 5]
-  square_array.each do |i|
-    puts i ** 2
+    [1, 2, 3].each do |n|
+      array = "Square number is: #{n}"
+      puts num ** 2
   end
+  
