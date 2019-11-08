b872c878d59a6d22697c74d1e4a14b82a64e4cfb
connor worthen <connorw220@protonmail.com>
2019-11-08 20:39:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,10 @@
-def square_array(array)
-  square_array = ["1", "2", "3"]
-  square_array.each do { |n| a << n ** n }
-    puts "The square is #{square_array}"
+  def square_array(array)
+    square_array = ["1", "2", "3"]
+    array.map do |num|
+      if (num.square?)
+        num
+      else
+        num ** 2
+      end
+    end
   end
