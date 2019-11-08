dcd4a51aa1f163437fd6eeab75416a7bbf556557
connor worthen <connorw220@protonmail.com>
2019-11-08 20:54:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  array.each([]) { |i,arr| arr << i ** 2 }
+  def square_array(array)
+    inject.new do |arr|
+      array.each([]) { |i,arr| arr << i ** 2 }
+    end
+    .take(array.length)
 end
+
+my arr = [1, 2, 3, 4]
+p square_array(my_arr)
