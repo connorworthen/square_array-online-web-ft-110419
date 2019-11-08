39fcfc735f35f8e7674b79781114830747e78df7
connor worthen <connorw220@protonmail.com>
2019-11-08 21:31:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,12 +3,8 @@
   end
 
   def sqaure_array(array)
-    array = ["1", "2", "3"]
-    array.map do |num|
-      if (num.square?)
-        num
-      else
-        num ** 2
-      end
+    array = [1, 2, 3, 4, 5]
+    array.each do |num|
+      puts num ** 2
+      sqaure_array << num**2
     end
-  end
