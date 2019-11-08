2f5cd43c7bb809226553cfdc5ed06811f8e40b7d
connor worthen <connorw220@protonmail.com>
2019-11-08 21:39:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,4 @@
-    def sqaure_array(array)
-      array = [1, 2, 3, 4, 5]
-    results_array = []
+  def sqaure_array(array)
     array.each do |num|
       puts num ** 2
-      results_array << num ** 2
-  end
-
-array = [1, 2, 3, 4, 5]
+    end
